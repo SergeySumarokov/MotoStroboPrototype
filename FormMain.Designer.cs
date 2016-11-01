77b5b90ddcab9931a57dcc1fb1b0ab1572a318ca
Sergey Sumarokov <sumarokov@nm.ru>
2016-11-01 09:43:27 +0300

    INIT#

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MotoStroboPrototype
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "FormMain";
+            this.Text = "Moto Strobo Prototype";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
